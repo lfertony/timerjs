796a2975cf10821e7efafaa8f4b8d58c316f99e7
Lucas Ferreira Pinto <Lucas.Ferreira.Pinto1@ibm.com>
2020-03-21 01:09:48 -0300

    script.js updated, style.css properties added

---
+++ countdownJS/script.js
@@ -0,0 +1,18 @@
+const cont = 1;
+let time = cont * 60 ;
+
+const countdownEL = document.getElementById('countdown');
+
+var down = setInterval(updateCount,1000);
+function updateCount(){
+    const min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    sec = sec < 10 ? '0' + sec: sec;
+    countdownEL.innerHTML=(`${min}:${sec}`);
+
+    if (time <=0){
+        clearInterval(down);
+        countdownEL.innerHTML=('Done');
+    }
+    time--;
+}
